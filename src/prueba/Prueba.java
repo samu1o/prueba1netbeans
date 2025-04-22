e03bfda0f9282653d2c2f90ed36377bc54bb1e28
samuel daniele fernandez <samueldaniele70@gmail.com>
2025-04-22 11:57:41 +0200

    primer

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,9 @@
+package prueba;
+public class Prueba {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("primer commit");
+    }
+    
+}
